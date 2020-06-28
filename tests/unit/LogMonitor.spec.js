a899fbb2c0e1039f84847832585eb257dbdf0906
msaltnet <salt.jeong@gmail.com>
2020-06-28 23:53:14 +0900

    Remove snapshot matching test

--- tests/unit/LogMonitor.spec.js
+++ tests/unit/LogMonitor.spec.js
@@ -4,31 +4,13 @@ import Vuetify from 'vuetify';
 
 // Utilities
 import {
-  mount,
-  createLocalVue
+  mount
 } from '@vue/test-utils'
 Vue.use(Vuetify);
 
-const localVue = createLocalVue()
 describe('LogMonitor.vue', () => {
-  let vuetify
-
-  beforeEach(() => {
-    vuetify = new Vuetify()
-  })
-
-  it('should have a default content and match snapshot', () => {
-    const wrapper = mount(LogMonitor, {
-      localVue,
-      vuetify,
-    })
-
-    // With jest we can create snapshot files of the HTML output
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
   it('should call registerListener', () => {
-    const vm = new Vue(LogMonitor).$mount();
+    const vm = mount(LogMonitor).vm;
     vm.isListenerOn = true;
     vm.logListener.registerListener = jest.fn();
 
@@ -37,7 +19,7 @@ describe('LogMonitor.vue', () => {
   })
 
   it('should call unregisterListener', () => {
-    const vm = new Vue(LogMonitor).$mount();
+    const vm = mount(LogMonitor).vm;
     vm.isListenerOn = false;
     vm.logListener.unregisterListener = jest.fn();
 
