f3a50051a255cee6fb4ce447763ea52f28abe098
msaltnet <salt.jeong@gmail.com>
2020-06-28 22:43:58 +0900

    Create onPowerEvent to control power

--- src/LogService.js
+++ src/LogService.js
@@ -1,15 +1,18 @@
 import { webContents } from 'electron';
 
 export default class {
-    constructor (ipcMain) {
+    constructor (ipcMain, sdbManager) {
         this.ipcMain = ipcMain;
         this.LISTEN_REQUEST_CHANNEL = "register-listener";
+        this.POWER_EVENT_CHANNEL = "change-power";
         this.listenerMap = new Map();
         this.testTimer = 0;
+        this.sdbManager = sdbManager;
     }
 
     init() {
         this.ipcMain.on(this.LISTEN_REQUEST_CHANNEL, this.onListenRegister.bind(this));
+        this.ipcMain.on(this.POWER_EVENT_CHANNEL, this.onPowerEvent.bind(this));
     }
 
     startTestMessage() {
@@ -31,6 +34,27 @@ export default class {
             console.log('Delete :' + event.sender.id);
             this.listenerMap.delete(event.sender.id);
         }
+    }
+
+    onPowerEvent(event, arg) {
+        if (arg && arg == 'start') {
+            this.setPower(true);
+        } else {
+            this.setPower(false);
+        }
+    }
+
+    setPower(on) {
+        if (!this.sdbManager) {
+            console.error('Invaild sdbManager');
+            return;
+        }
+
+        console.log('setPower :' + on);
+        if (on)
+            this.sdbManager.startDlog();
+        else
+            this.sdbManager.stopDlog();
     }
 
     sendTestMessage() {
--- tests/unit/LogService.spec.js
+++ tests/unit/LogService.spec.js
@@ -11,10 +11,10 @@ describe('LogService', () => {
         const logService = new LogService(mockIpcMain);
 
         logService.init();
-        expect(mockIpcMain.on).toBeCalledWith(logService.LISTEN_REQUEST_CHANNEL, expect.anything());
+        expect(mockIpcMain.on).toBeCalledWith(logService.LISTEN_REQUEST_CHANNEL, expect.any(Function));
     })
 
-    it('should have a listener after onListenRegister with channel info', () => {
+    it('should have a listener after onListenRegister called with channel info', () => {
         const mockIpcMain = {
             on: jest.fn(),
         }
@@ -34,7 +34,7 @@ describe('LogService', () => {
         expect(logService.listenerMap.get(mockListener.sender.id).channel).toEqual(mockChannel);
     })
 
-    it('should delete listener after onListenRegister with null channel info', () => {
+    it('should delete listener after onListenRegister called with null channel info', () => {
         const mockIpcMain = {
             on: jest.fn(),
         }
@@ -51,4 +51,67 @@ describe('LogService', () => {
         logService.onListenRegister(mockListener, null);
         expect(logService.listenerMap.size).toEqual(0);
     })
+
+    it('should register onPowerEvent callback to ipcMain', () => {
+        const mockIpcMain = {
+            on: jest.fn(),
+        }
+        const logService = new LogService(mockIpcMain);
+
+        logService.init();
+        expect(mockIpcMain.on).toBeCalledWith(logService.POWER_EVENT_CHANNEL, expect.any(Function));
+    })
+
+    it('should call setPower with true when onPowerEvent called with "start" argument', () => {
+        const mockIpcMain = {
+            on: jest.fn(),
+        }
+        const mockSdbManager = {
+            startDlog: jest.fn(),
+            stopDlog: jest.fn()
+        }
+        const logService = new LogService(mockIpcMain, mockSdbManager);
+        const mockListener = {
+            sender: {
+                id: "monkey"
+            }
+        };
+
+        const mockSetPower = jest.fn();
+        logService.setPower = mockSetPower;
+        logService.onPowerEvent(mockListener, "start");
+        expect(mockSetPower).toBeCalledWith(true);
+    })
+
+    it('should call setPower with false when onPowerEvent called without "start" argument', () => {
+        const mockIpcMain = {
+            on: jest.fn(),
+        }
+        const mockSdbManager = {
+            startDlog: jest.fn(),
+            stopDlog: jest.fn()
+        }
+        const logService = new LogService(mockIpcMain, mockSdbManager);
+        const mockListener = {
+            sender: {
+                id: "monkey"
+            }
+        };
+
+        let mockSetPower = jest.fn();
+        logService.setPower = mockSetPower;
+        logService.onPowerEvent(mockListener, "star");
+        expect(mockSetPower).toBeCalledWith(false);
+
+        let mockSetPower2 = jest.fn();
+        logService.setPower = mockSetPower2;
+        logService.onPowerEvent(mockListener, "");
+        expect(mockSetPower2).toBeCalledWith(false);
+
+        let mockSetPower3 = jest.fn();
+        logService.setPower = mockSetPower3;
+        logService.onPowerEvent(mockListener);
+        expect(mockSetPower3).toBeCalledWith(false);
+    })
+
 })
