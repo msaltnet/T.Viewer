8c877e02f0eca2463ef450885bead40f05e33ee0
msaltnet <salt.jeong@gmail.com>
2020-08-05 22:01:44 +0900

    up version 1.0.1

--- src/menuTemplate.js
+++ src/menuTemplate.js
@@ -46,7 +46,7 @@ const template = [
                         type: "info",
                         title: "T.Viewer",
                         message: "T.Viewer",
-                        detail: "Version: 1.0.0\nDate: 2020. 8. 4"
+                        detail: "Version: 1.0.1\nDate: 2020. 8. 5"
                     });
                 }
             }
